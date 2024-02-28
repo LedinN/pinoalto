ac192fb7054bf65762d04b445b420a6dc5fe8e5e
Ledin Nick (DINBIL - SE/Hammarby) <Nick.Ledin@dinbil.se>
2024-02-28 14:14:51 +0100

    added stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ function App() {
 
   return (
     <>
-    <div className='navbar'>
-    <nav>
-      <p>test</p>
-    </nav>
-    </div>
+    <nav className="navbar">
+  <a href="#home" className="logo">Pino Alto 3</a>
+  <div className="nav-links">
+    <a href="#services">Pictures</a>
+    <a href="#contact">Contact</a>
+  </div>
+</nav>
      test2
     </>
   )
