9c259ae60705922117ec6bb5031a5d7ac767d0ac
Ledin Nick (DINBIL - SE/Hammarby) <Nick.Ledin@dinbil.se>
2024-02-28 11:57:32 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='navbar'>
+    <nav>
+      <p>test</p>
+    </nav>
+    </div>
+     test2
+    </>
+  )
+}
+
+export default App
